65245fdd2be34f83407dfc843c6c8a3342ddc719
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-16 16:25:57 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_song(name, genre)
+    name = Song.new(name, self, genre)
+  end 
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end 
+  
+  def genres 
+    Song.all.select do |song|
+      if song.artist == self 
+        song.genre 
+      end 
+    end 
+  end 
+  
+  
+  
+end 
